88a536d198eb0801c06caf6d74e8f2627bb5a5b5
vsirik <vadimss.dremin@gmail.com>
2025-03-22 15:15:56 +0700

    fix

--- src/main/java/TestLogger.java
+++ src/main/java/TestLogger.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class TestLogger {
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class TestLogger {
+
+    private static final Logger logger = LogManager.getLogger(TestLogger.class);
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "localhost:9092";
+        String topic = "app-logs";
+        String groupId = "log-listener-group";
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singleton(topic));
+
+        System.out.println("Запускаем listener для топика: " + topic);
+
+
+        logger.info("Это тестовое сообщение для Kafka!");
+
+        try {
+            boolean shouldRun = true;
+            while (shouldRun) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Получено сообщение: offset = %d, key = %s, value = %s%n",
+                            record.offset(), record.key(), record.value());
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+
 }
